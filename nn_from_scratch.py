471e8550f4251ca18e75d556de66ad5de8c7b553
Gowtham Behara <gothsiddu@gmail.com>
2018-12-03 11:54:10 +0530

    Implement func to calc output given the input to nn

--- nn_from_scratch.py
+++ nn_from_scratch.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 
+def sigmoid(z): return 1 / (1 + np.exp(-z))
+
+
 class Network(object):
     def __init__(self, layers):
         self.num_of_layers = len(layers)
         self.layers = layers
         self.bias = [np.random.randn(nodes, 1) for nodes in layers[1:]]
         self.weights = [np.random.randn(x, y) for x, y in zip(layers[1:], layers[:-1])]
+
+    def feed_forward(self, x):
+        """Return output of the nn, given the input"""
+        for bias, weights in zip(self.bias, self.weights):
+            x = sigmoid(np.dot(weights, x) + bias)
+
+        return x
