18470aa796e13388be444ad7b5ba443fa08b5cab
Gowtham Behara <gothsiddu@gmail.com>
2018-12-04 12:43:49 +0530

    Implement stochatistic gradient descendent function

--- nn_from_scratch.py
+++ nn_from_scratch.py
@@ -17,3 +17,24 @@ class Network(object):
             x = sigmoid(np.dot(weights, x) + bias)
 
         return x
+
+    def update_mini_batch(self, mini_batch, learning_rate):
+        pass
+
+    def evaluate(self, test_data):
+        pass
+
+    def SGD(self, train_data, learning_rate, epochs, mini_batch_size, test_data=None):
+        if test_data:
+            test_data_len = len(test_data)
+
+        train_data_len = len(train_data)
+
+        for epoch in range(epochs):
+            mini_batches = [train_data[k: k + mini_batch_size] for k in range(0, train_data_len, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, learning_rate)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(epoch, self.evaluate(test_data), test_data_len))
+            else:
+                print("Epoch {0} complete".format(epoch))
