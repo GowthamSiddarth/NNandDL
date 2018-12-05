e729332e751aedc47917d837073661fdd6aeddf0
Gowtham Behara <gothsiddu@gmail.com>
2018-12-05 08:49:37 +0530

    Run aNN with loading mnist data

---
+++ hand_written_classification.py
@@ -0,0 +1,6 @@
+import mnist_loader, ann
+
+train_data, validation_data, test_data = mnist_loader.load_data_wrapper("data/mnist.pkl.gz")
+network = ann.Network([784, 30, 10])
+learning_rate, epochs, mini_batch_size = 3.0, 30, 10
+network.stochastic_gradient_descent(train_data, learning_rate, epochs, mini_batch_size, test_data)
